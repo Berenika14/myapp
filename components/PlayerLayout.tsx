8a526cc905b4607dda9f8cd6ccfaad93bf4f5d46
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-05-18 23:00:09 -0400

    added the logo

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -5,7 +5,7 @@ const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        <Sidebar></Sidebar>
+        <Sidebar />
       </Box>
       <Box marginLeft="250px" marginBottom="100px">
         {children}
